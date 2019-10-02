acb752bc93a98323690ebe8f4519d976b8571331
nobug <sheldonbernstein2k.gmail.com>
2019-10-02 14:23:12 -0400

    bug fixed

--- dual_quat_library.py
+++ dual_quat_library.py
@@ -44,10 +44,10 @@ class Quaternion(object):
 
     # Get Axis-Angle representation of rotation matrix
     def get_axis_angle(self):
-        new_q = self.total/np.dot(self.total, self.total)
+        new_q = self.total/np.sqrt(np.dot(self.total, self.total))
         # print(new_q)
         if new_q[0] == 1:
-            # raise Exception("scalar part can not be zero\n")
+            # raise Exception("q0=1\n")
             ang = 0
             ax = np.array([1, 0, 0])
         else:
@@ -57,7 +57,7 @@ class Quaternion(object):
 
     # Get rotation matrix
     def get_rotation_matrix(self):
-        nq = self.total / np.dot(self.total, self.total)
+        nq = self.total / np.sqrt(np.dot(self.total, self.total))
         return np.array([[1-2*(nq[2]**2 + nq[3]**2), 2*(nq[1]*nq[2]-nq[3]*nq[0]), 2*(nq[1]*nq[3]+nq[2]*nq[0])],
                          [2*(nq[1]*nq[2]+nq[3]*nq[0]), 1-2*(nq[1]**2 + nq[3]**2), 2*(nq[2]*nq[3]-nq[1]*nq[0])],
                          [2*(nq[1]*nq[3]-nq[2]*nq[0]), 2*(nq[2]*nq[3]+nq[1]*nq[0]), 1-2*(nq[1]**2 + nq[2]**2)]])
@@ -119,6 +119,123 @@ class DualQuaternion(object):
         q_dual = (self.real * other.dual) + (self.dual * other.real)
         return DualQuaternion(q_real, q_dual)
 
+
+# Class to encapsulate screw parameters
+class ScrewParam(object):
+    def __init__(self, u, th, d, m):
+        self.u = u
+        self.th = th
+        self.d = d
+        self.m = m
+
+
+# Class to interpolate between Rigid-Displacements in Dual-Quaternion form
+class IntpRigidDispDualQuat(object):
+    def __init__(self, p_i, q_i, p_f, q_f):
+        self.initial_config = self.convert_dual_quat(p_i, q_i)
+        self.final_config = self.convert_dual_quat(p_f, q_f)
+        self.relative_config = self.initial_config.get_conjugate() * self.final_config
+        temp_dir = self.relative_config.dual.vector
+        self.translation_dir = temp_dir/np.sqrt(np.dot(temp_dir, temp_dir))
+
+    # Given a Dual-Quaternion return its Screw parameters(u, th, d, m)
+    def compute_screw_param(self, q):
+        u, th = q.real.get_axis_angle()
+        if th == 0:
+            u = self.translation_dir
+        d = self.compute_screw_d(q, u)
+        temp = q.dual * q.real.conjugate_quat()
+        t = 2 * temp.vector
+        m = self.compute_screw_m(u, th, d, t)
+        return ScrewParam(u, th, d, m)
+
+    def interpolate(self):
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection='3d')
+        sp = self.compute_screw_param(self.relative_config)
+        tau_list = np.linspace(0, 1, num=100)
+        for idx, tau in enumerate(tau_list):
+            temp = self.pow_dual_quat(sp, tau)
+            dual_tau = self.initial_config * self.pow_dual_quat(sp, tau)
+            if idx % 3 == 0 or idx == 0:
+                print("Iteration %d" % idx)
+                print("Intermediate Config (Dual-Quaternion):")
+                print(dual_tau)
+
+                position = 2 * (dual_tau.dual * dual_tau.real.conjugate_quat()).vector
+                rotation_matrix = dual_tau.real.get_rotation_matrix()
+                print("Intermediate Position (Vector form):")
+                print(position)
+
+                print("Intermediate Rotation (Matrix form):")
+                print(rotation_matrix)
+
+                print("\n")
+
+                # Plot
+                ax.quiver(position[0], position[1], position[2], rotation_matrix[0, 0], rotation_matrix[1, 0],
+                          rotation_matrix[2, 0], length=0.01, normalize=False, color='r')
+                ax.quiver(position[0], position[1], position[2], rotation_matrix[0, 1], rotation_matrix[1, 1],
+                          rotation_matrix[2, 1], length=0.01, normalize=False, color='g')
+                ax.quiver(position[0], position[1], position[2], rotation_matrix[0, 2], rotation_matrix[1, 2],
+                          rotation_matrix[2, 2], length=0.01, normalize=False, color='b')
+        ax.set_xlabel("x [m]")
+        ax.set_ylabel("y [m]")
+        ax.set_zlabel("z [m]")
+        ax.set_title("Screw-Interpolation")
+        # ax.set_aspect('equal')
+        ax.view_init(10, 130)
+        plt.show()
+
+    # Convert Position Vector and Orientation Quaternion into Dual-Quaternion
+    @staticmethod
+    def convert_dual_quat(p, q):
+        real = Quaternion(q)
+        u, th = real.get_axis_angle()
+        scalar_part = -np.sin(th / 2) * np.dot(p, u)
+        vector_part = np.cos(th / 2) * p + np.sin(th / 2) * np.cross(p, u)
+        total_vec = np.hstack((scalar_part, vector_part))
+        dual = Quaternion(0.5 * total_vec)
+        return DualQuaternion(real, dual)
+
+    # Power of a dual-quaternion
+    @staticmethod
+    def pow_dual_quat(screw_param, tau):
+        real_vec = np.hstack((np.cos(tau * screw_param.th / 2), np.sin(tau * screw_param.th / 2) * screw_param.u))
+        dual_vec = np.hstack((-tau * screw_param.d * np.sin(tau * screw_param.th / 2) / 2,
+                              np.sin(tau * screw_param.th / 2) * screw_param.m + tau * screw_param.d * np.cos(
+                                  tau * screw_param.th / 2) * screw_param.u / 2))
+        return DualQuaternion(Quaternion(real_vec), Quaternion(dual_vec))
+
+    # Compute d of screw parameter
+    @staticmethod
+    def compute_screw_d(dual_quat, u):
+        temp = dual_quat.dual * dual_quat.real.conjugate_quat()
+        return 2 * np.dot(temp.vector, u)
+
+    # Compute m of screw parameter
+    @staticmethod
+    def compute_screw_m(u, th, d, t):
+        if th != 0:
+            temp1 = np.cross(t, u)
+            temp2 = (t - d * u) / np.tan(th / 2)
+            temp = temp1 + temp2
+            return 0.5 * temp
+        else:
+            return np.array([0, 0, 0])
+
+
+# Rotation matrix to quaternion
+def rotm2quat(rotm):
+    if (rotm[0, 0] + rotm[1, 1] + rotm[2, 2] - 1) / 2 < -1 or (rotm[0, 0] + rotm[1, 1] + rotm[2, 2] - 1) / 2 > 1:
+        raise Exception("Singular configuration to convert rotation matrix to quaternion")
+    angle = np.arccos((rotm[0, 0] + rotm[1, 1] + rotm[2, 2] - 1) / 2)
+    temp = np.sqrt((rotm[2, 1] - rotm[1, 2])**2 + (rotm[0, 2] - rotm[2, 0])**2 + (rotm[1, 0] - rotm[0, 1])**2)
+    wx = (rotm[2, 1] - rotm[1, 2]) / temp
+    wy = (rotm[0, 2] - rotm[2, 0]) / temp
+    wz = (rotm[1, 0] - rotm[0, 1]) / temp
+    quat = np.array([np.cos(angle / 2), wx * np.sin(angle / 2), wy * np.sin(angle / 2), wz * np.sin(angle / 2)])
+    return quat
 
 # Class to deal Rigid-Displacements with Dual-Quaternions
 # class Homo2DualQuat(object):
@@ -184,125 +301,6 @@ class DualQuaternion(object):
 #         th = 2 * np.arccos(qin[0])
 #         u = qin[1:]/np.sqrt(1 - qin[0]**2)
 #         return u, th
-
-
-# Class to encapsulate screw parameters
-class ScrewParam(object):
-    def __init__(self, u, th, d, m):
-        self.u = u
-        self.th = th
-        self.d = d
-        self.m = m
-
-
-# Class to interpolate between Rigid-Displacements in Dual-Quaternion form
-class IntpRigidDispDualQuat(object):
-    def __init__(self, p_i, q_i, p_f, q_f):
-        self.initial_config = self.convert_dual_quat(p_i, q_i)
-        self.final_config = self.convert_dual_quat(p_f, q_f)
-        self.relative_config = self.initial_config.get_conjugate() * self.final_config
-
-    # Given a Dual-Quaternion return its Screw parameters(u, th, d, m)
-    def compute_screw_param(self, q):
-        u, th = q.real.get_axis_angle()
-        # u, th = self.Quat2AxisAng(q.real)
-        d = self.compute_screw_d(q, u)
-        temp = q.dual * q.real.conjugate_quat()
-        t = 2 * temp.vector
-        m = self.compute_screw_m(u, th, d, t)
-        return ScrewParam(u, th, d, m)
-
-    def interpolate(self):
-        fig = plt.figure(figsize=(8, 6))
-        ax = fig.add_subplot(111, projection='3d')
-
-        sp = self.compute_screw_param(self.relative_config)
-        # p_initial = 2 * (self.initial_config.dual * self.initial_config.real.conjugate_quat()).vector
-        # print("\nInitial Position")
-        # print(p_initial)
-        tau_list = np.linspace(0, 1, num=100)
-        for idx, tau in enumerate(tau_list):
-            dual_tau = self.initial_config * self.pow_dual_quat(sp, tau)
-            if idx % 3 == 0 or idx == 0:
-                print("Iteration %d" % idx)
-                print("Intermediate Config (Dual-Quaternion):")
-                print(dual_tau)
-
-                position = 2 * (dual_tau.dual * dual_tau.real.conjugate_quat()).vector
-                rotation_matrix = dual_tau.real.get_rotation_matrix()
-                print("Intermediate Position (Vector form):")
-                print(position)
-
-                print("Intermediate Rotation (Matrix form):")
-                print(rotation_matrix)
-
-                print("\n")
-
-                # Plot
-                ax.quiver(position[0], position[1], position[2], rotation_matrix[0, 0], rotation_matrix[1, 0],
-                          rotation_matrix[2, 0], length=0.01, normalize=False, color='r')
-                ax.quiver(position[0], position[1], position[2], rotation_matrix[0, 1], rotation_matrix[1, 1],
-                          rotation_matrix[2, 1], length=0.01, normalize=False, color='g')
-                ax.quiver(position[0], position[1], position[2], rotation_matrix[0, 2], rotation_matrix[1, 2],
-                          rotation_matrix[2, 2], length=0.01, normalize=False, color='b')
-        ax.set_xlabel("x [m]")
-        ax.set_ylabel("y [m]")
-        ax.set_zlabel("z [m]")
-        ax.set_title("Screw-Interpolation")
-        # ax.set_aspect('equal')
-        ax.view_init(10, 130)
-        plt.show()
-
-    # Convert Position Vector and Orientation Quaternion into Dual-Quaternion
-    @staticmethod
-    def convert_dual_quat(p, q):
-        real = Quaternion(q)
-        # u, th = self.Quat2AxisAng(real)
-        u, th = real.get_axis_angle()
-        scalar_part = -np.sin(th / 2) * np.dot(p, u)
-        vector_part = np.cos(th / 2) * p + np.sin(th / 2) * np.cross(p, u)
-        total_vec = np.hstack((scalar_part, vector_part))
-        dual = Quaternion(0.5 * total_vec)
-        return DualQuaternion(real, dual)
-
-    # Power of a dual-quaternion
-    @staticmethod
-    def pow_dual_quat(screw_param, tau):
-        real_vec = np.hstack((np.cos(tau * screw_param.th / 2), np.sin(tau * screw_param.th / 2) * screw_param.u))
-        dual_vec = np.hstack((-tau * screw_param.d * np.sin(tau * screw_param.th / 2) / 2,
-                              np.sin(tau * screw_param.th / 2) * screw_param.m + tau * screw_param.d * np.cos(
-                                  tau * screw_param.th / 2) * screw_param.u / 2))
-        return DualQuaternion(Quaternion(real_vec), Quaternion(dual_vec))
-
-    # Compute d of screw parameter
-    @staticmethod
-    def compute_screw_d(dual_quat, u):
-        temp = dual_quat.dual * dual_quat.real.conjugate_quat()
-        return 2 * np.dot(temp.vector, u)
-
-    # Compute m of screw parameter
-    @staticmethod
-    def compute_screw_m(u, th, d, t):
-        if th != 0:
-            temp1 = np.cross(t, u)
-            temp2 = (t - d * u) / np.tan(th / 2)
-            temp = temp1 + temp2
-            return 0.5 * temp
-        else:
-            return np.array([0, 0, 0])
-
-
-# Rotation matrix to quaternion
-def rotm2quat(rotm):
-    if (rotm[0, 0] + rotm[1, 1] + rotm[2, 2] - 1) / 2 < -1 or (rotm[0, 0] + rotm[1, 1] + rotm[2, 2] - 1) / 2 > 1:
-        raise Exception("Singular configuration to convert rotation matrix to quaternion")
-    angle = np.arccos((rotm[0, 0] + rotm[1, 1] + rotm[2, 2] - 1) / 2)
-    temp = np.sqrt((rotm[2, 1] - rotm[1, 2])**2 + (rotm[0, 2] - rotm[2, 0])**2 + (rotm[1, 0] - rotm[0, 1])**2)
-    wx = (rotm[2, 1] - rotm[1, 2]) / temp
-    wy = (rotm[0, 2] - rotm[2, 0]) / temp
-    wz = (rotm[1, 0] - rotm[0, 1]) / temp
-    quat = np.array([np.cos(angle / 2), wx * np.sin(angle / 2), wy * np.sin(angle / 2), wz * np.sin(angle / 2)])
-    return quat
 
 
 # Test cases
